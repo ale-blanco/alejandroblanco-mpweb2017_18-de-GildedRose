fd6a4be02e611d6128e343a353fe962f63dccc64
alejandroblanco <alejandroblancoluna@gmail.com>
2018-01-21 16:43:15 +0100

    Aplicadas clausulas de guarda

--- src/GildedRose/GildedRose.php
+++ src/GildedRose/GildedRose.php
@@ -12,6 +12,8 @@ class GildedRose
     const FIRST_TIME_LIMIT = 10;
     const SECOND_TIME_LIMIT = 5;
     const VARIATION_QUALITY = 1;
+    const DOWN_LIMIT_SELLIN = 0;
+    const DOWN_LIMIT_QUALITY = 0;
 
     private $items;
 
@@ -31,56 +33,64 @@ class GildedRose
 
     private function updateItemQuality(Item $item): void
     {
-        if ($item->name != self::ITEM_AGEDBRIE and $item->name != self::ITEM_BACKSTAGE) {
-            if ($item->quality > 0) {
-                if ($item->name != self::ITEM_SULFURAS) {
-                    $item->quality = $item->quality - self::VARIATION_QUALITY;
-                }
-            }
-        } else {
-            if ($item->quality < self::UP_LIMIT) {
-                $item->quality = $item->quality + self::VARIATION_QUALITY;
-                if ($item->name == self::ITEM_BACKSTAGE) {
-                    if ($item->sell_in <= self::FIRST_TIME_LIMIT) {
-                        if ($item->quality < self::UP_LIMIT) {
-                            $item->quality = $item->quality + self::VARIATION_QUALITY;
-                        }
-                    }
-                    if ($item->sell_in <= self::SECOND_TIME_LIMIT) {
-                        if ($item->quality < self::UP_LIMIT) {
-                            $item->quality = $item->quality + self::VARIATION_QUALITY;
-                        }
-                    }
-                }
-            }
+        if ($item->name != self::ITEM_AGEDBRIE && $item->name != self::ITEM_BACKSTAGE
+            && $item->name != self::ITEM_SULFURAS && $item->quality > self::DOWN_LIMIT_QUALITY
+        ) {
+            $item->quality = $item->quality - self::VARIATION_QUALITY;
+            return;
+        }
+
+        if ($item->quality >= self::UP_LIMIT) {
+            return;
+        }
+
+        $item->quality = $item->quality + self::VARIATION_QUALITY;
+
+        if ($item->quality >= self::UP_LIMIT) {
+            return;
+        }
+
+        if ($item->name != self::ITEM_BACKSTAGE) {
+            return;
+        }
+
+        if ($item->sell_in <= self::FIRST_TIME_LIMIT) {
+            $item->quality = $item->quality + self::VARIATION_QUALITY;
+        }
+
+        if ($item->sell_in <= self::SECOND_TIME_LIMIT) {
+            $item->quality = $item->quality + self::VARIATION_QUALITY;
         }
     }
 
     private function updateItemSellIn(Item $item): void
     {
-        if ($item->name != self::ITEM_SULFURAS) {
-            $item->sell_in = $item->sell_in - self::VARIATION_QUALITY;
+        if ($item->name === self::ITEM_SULFURAS) {
+            return;
         }
+
+        $item->sell_in = $item->sell_in - self::VARIATION_QUALITY;
     }
 
     private function checkSellInAndUpdateQuality(Item $item): void
     {
-        if ($item->sell_in < 0) {
-            if ($item->name != self::ITEM_AGEDBRIE) {
-                if ($item->name != self::ITEM_BACKSTAGE) {
-                    if ($item->quality > 0) {
-                        if ($item->name != self::ITEM_SULFURAS) {
-                            $item->quality = $item->quality - self::VARIATION_QUALITY;
-                        }
-                    }
-                } else {
-                    $item->quality = $item->quality - $item->quality;
-                }
-            } else {
-                if ($item->quality < self::UP_LIMIT) {
-                    $item->quality = $item->quality + self::VARIATION_QUALITY;
-                }
-            }
+        if ($item->sell_in >= self::DOWN_LIMIT_SELLIN) {
+            return;
         }
+
+        if ($item->name === self::ITEM_AGEDBRIE && $item->quality < self::UP_LIMIT) {
+            $item->quality = $item->quality + self::VARIATION_QUALITY;
+            return;
+        }
+
+        if ($item->name === self::ITEM_BACKSTAGE) {
+            $item->quality = $item->quality - $item->quality;
+        }
+
+        if ($item->quality <= self::DOWN_LIMIT_QUALITY || $item->name === self::ITEM_SULFURAS) {
+            return;
+        }
+
+        $item->quality = $item->quality - self::VARIATION_QUALITY;
     }
 }
